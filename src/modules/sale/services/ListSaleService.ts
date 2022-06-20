3cf4894071912387eff7e7752165a7bf90e0a79a
gustavomenezesh <menezesguga@gmail.com>
2022-06-20 17:33:07 -0300

    Fizing filter list sales

--- src/modules/sale/services/ListSaleService.ts
+++ src/modules/sale/services/ListSaleService.ts
@@ -9,7 +9,7 @@ class CreateProductService {
     @inject(Types.SaleRepository) private saleRepository!: ISaleRepository;
 
     public async execute(userId: string): Promise<Sale[]> {
-      const [sales] = await this.saleRepository.list({ userId }, ['delivery', 'adress', 'saleProducts', 'saleProducts.product', 'saleProducts.size', 'saleProducts.personalization']);
+      const [sales] = await this.saleRepository.list(userId ? { userId, enabled: true } : { enabled: true }, ['delivery', 'adress', 'saleProducts', 'saleProducts.product', 'saleProducts.size', 'saleProducts.personalization']);
       return sales;
     }
 }
